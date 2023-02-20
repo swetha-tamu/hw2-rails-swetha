5066036b31751e57b411129286a088be1522e262
swethasangem <swethareddysangem@gmail.com>
2023-02-20 13:50:29 -0600

    made changes to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
   {:title => 'The Imitation Game', :rating => 'PG-13',
     :release_date => '28-Sept-2014'}
 ]
